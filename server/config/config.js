3718a8a4cebfb52f2116ae53c29c24a914ac6c85
Sean Boisselle <sboisselle@ufl.edu>
2017-09-26 17:33:14 -0400

    Create config.js

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://seanboisselle:Mongo55@ds127044.mlab.com:27044/smb', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: 'https://maps.googleapis.com/maps/api/geocode/json'
+  },
+  port: 8080
+};
